10fb0e95464ae7ec47ee4e246d6aecb231372424
Arthur Plazanet <plazanet.art@gmail.com>
2025-04-04 06:18:27 +0200

    Fix package json

--- build/index.d.ts
+++ build/index.d.ts
@@ -2,5 +2,3 @@ export interface AppConfig {
     key: string;
     name: string;
 }
-export type Constructor<T = any> = new (...args: any[]) => T;
-export type Class<T = any> = InstanceType<Constructor<T>>;
